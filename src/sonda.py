795998b72bc353c6e4ff28ce20457e2ab2ea5928
mateuskywalker <mhenrique977@gmail.com>
2018-01-11 12:45:09 -0200

    Implementacao do move
    Implementacao do move completa

--- src/sonda.py
+++ src/sonda.py
@@ -2,18 +2,6 @@ print("Bem-Vindo ao Move Sonda")
 print("-------=========------- \n")
 
 # funcoes de movimento da sonda
-def executaComandoY(comando, direction, y):
-
-    if comando == 'M' and direction == 'N':
-
-        y += 1
-
-    elif comando == 'M' and direction == 'S':
-
-        y -= 1
-
-    print("Y: {}".format(y))
-
 def executaComandoX(comando, direction, x):
 
     if comando == 'M' and direction == 'E':
@@ -24,27 +12,45 @@ def executaComandoX(comando, direction, x):
 
         x -= 1
 
-    print("X: {}".format(x))
+    #print("X: {}".format(x))
+    return x
 
+def executaComandoY(comando, direction, y):
+
+    if comando == 'M' and direction == 'N':
+
+        y += 1
+
+    elif comando == 'M' and direction == 'S':
+
+        y -= 1
+
+    #print("Y: {}".format(y))
+    return y
 
 x = int(input("Digite a posicao inicial no eixo x: "))
 y = int(input("Digite a posicao inicial no eixo y: "))
 direction = input("Digite a direcao da sonda: ")
 direction = direction.upper()
-print(direction)
+
+newX = x
+newY = y
 
 i = 1
 while(i != 0):
+
+
 
     comando = input("Digite um comando para nave: ")
     comando = comando.upper()
 
     if comando == 'M':
 
-        executaComandoX(comando, direction, x)
-        executaComandoY(comando, direction, y)
+        resultX = executaComandoX(comando, direction, newX)
+        newX = resultX
+        resultY = executaComandoY(comando, direction, newY)
+        newY = resultY
+
+    print("--X: {}, Y: {}--".format(resultX, resultY))
 
     i = int(input("Digite 0 para sair da tela de comandos ou qualquer tecla para continuar: "))
-
-
-    # print("{}, {}".format(newX, newY))
